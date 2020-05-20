9e44fd6ff354066d138501532c86cec0c30efc9a
Artur <arturtres3@gmail.com>
2020-05-20 02:35:28 -0300

    Download JSON and All Tweets link

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ app.get('/', (req, res) => {
     res.render('index.ejs', {tweet: tweets[Math.ceil(Math.random() * tweets.length)]});
   });
 
+app.get('/Download', (req, res) => {
+  var json = JSON.stringify(tweets);
+  var filename = 'dril.json';
+  var mimetype = 'application/json';
+  res.setHeader('Content-Type', mimetype);
+  res.setHeader('Content-disposition','attachment; filename='+filename);
+  res.send( json );
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 5000;
