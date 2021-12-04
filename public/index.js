d0ebec017a357557ffcc55cb582f32a0640d31d8
arturtres3 <32775795+arturtres3@users.noreply.github.com>
2021-12-04 19:20:10 -0300

    Return button (#6)
    * Cria historico
    
    * History and back buttons functional
    
    Uses a class History to manage history of ids
    
    * Visual Overhaul
    
    Fixed buttons for navigation. About and download buttons placed on navbar
    
    Co-authored-by: Artur <atamaral@idejr.com.br>

--- public/index.js
+++ public/index.js
@@ -1,16 +1,30 @@
 const next = document.getElementById('next')
+const back = document.getElementById('back')
+const container = document.getElementById('tweet-container')
+
+let history = new History()
+
+window.onload = () => {
+    // salva o primeiro id
+    history.add(container.children[0].children[0].href.replace("http://twitter.com/dril/status/", ""));
+}
+
 
 document.addEventListener('keyup', function (event) {
     if (event.defaultPrevented) {
         return;
     }
 
-    var key = event.key || event.keyCode;
+    let key = event.key || event.keyCode;
 
-    if (key === 'Enter' || key === 13) {
+    if (key === 'Enter' || key === 13){
         next.click()
     }
-});
+    if (key === 'Backspace' || key === 8){
+        back.click()
+    }
+
+})
 
 // recebe um novo tweet da rota /next (funcao copiada)
 function httpGetAsync(theUrl, callback)
@@ -24,23 +38,47 @@ function httpGetAsync(theUrl, callback)
     xmlHttp.send(null);
 }
 
+function loadNewTweet(novoId) {
+    const newTweet = document.createElement('blockquote')
+    newTweet.className = 'twitter-tweet'
 
-const container = document.getElementById('tweet-container')
+    const linkNewTweet = document.createElement('a')
+    linkNewTweet.href = 'http://twitter.com/dril/status/' + novoId
+    newTweet.appendChild(linkNewTweet)
+    
+    Array.from(container.children).forEach( oldTweet => {oldTweet.remove()} )  
+    container.appendChild(newTweet)
+    
+    twttr.widgets.load(container)
+}
 
 next.addEventListener('click', () => {
-    httpGetAsync('/next', (data) =>{
-        const newTweet = document.createElement('blockquote')
-        newTweet.className = 'twitter-tweet'
+    if(history.isOnTop()){
+        httpGetAsync('/next', (nextTweet) => { 
+            const id = JSON.parse(nextTweet).id
+            loadNewTweet(id) 
+            history.add(id)
+        })
+    }
+    else{ 
+        loadNewTweet(history.goForward())
+        if(history.isOnTop())
+            next.innerHTML = "Another one"
+    }
 
-        const linkNewTweet = document.createElement('a')
-        linkNewTweet.href = 'http://twitter.com/dril/status/' + JSON.parse(data).id
-        newTweet.appendChild(linkNewTweet)
-    
-        Array.from(container.children).forEach( oldTweet => {oldTweet.remove()} )  
-        container.appendChild(newTweet)
-        
-        twttr.widgets.load(container)
-    }) 
+    back.classList.remove("disable")
 })
+
+back.addEventListener('click', () => {
+    if(!history.isOnEnd()){
+        loadNewTweet(history.goBack())
+
+        if(history.isOnEnd())
+            back.classList.add("disable")
+
+        next.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Next &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+    }
+})
+
 
 
